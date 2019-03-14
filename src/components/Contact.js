ad4e5581eb6d4a1b13ed4eee885806111d27547c
Boi Soth <jellybahh@gmail.com>
2019-03-14 16:05:58 -0700

    Testing ssh

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = props => {
     <div className="card card-body mb-3">
       <h4>{name}</h4>
       <ul className="list-group">
-        <li className="list-group-item">Email: {email}</li>
+        <li className="list-group-item">Email!: {email}</li>
         <li className="list-group-item">Phone: {phone}</li>
       </ul>
     </div>
